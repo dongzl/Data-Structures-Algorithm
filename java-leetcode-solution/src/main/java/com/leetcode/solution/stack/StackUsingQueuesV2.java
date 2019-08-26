94532fce5a817e60573df6ed5d3bf65d55d17c01
dongzl <dzllikelsw@163.com>
2019-08-26 19:18:29 +0800

    Leetcode 225 implement-stack-using-queues

--- java-leetcode-solution/src/main/java/com/leetcode/solution/stack/StackUsingQueuesV2.java
+++ java-leetcode-solution/src/main/java/com/leetcode/solution/stack/StackUsingQueuesV2.java
@@ -2,14 +2,13 @@ package com.leetcode.solution.stack;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author dongzonglei
  * @description
  * @date 2019-08-26 17:26
  */
-public class StackUsingQueues {
+public class StackUsingQueuesV2 {
 
     private Queue<Integer> q1 = new LinkedList<>();
 
@@ -18,31 +17,33 @@ public class StackUsingQueues {
     private int top;
 
     /** Initialize your data structure here. */
-    public StackUsingQueues() {
+    public StackUsingQueuesV2() {
     }
 
     /** Push element x onto stack. */
     public void push(int x) {
-        q1.add(x);
+        q2.add(x);
         top = x;
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q2;
+        q2 = q1;
+        q1 = temp;
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        while (q1.size() > 1) {
-            top = q1.remove();
-            q2.add(top);
-        }
         int result = q1.remove();
-        Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
+        if (!q1.isEmpty()) {
+            top = q1.peek();
+        }
         return result;
     }
 
     /** Get the top element. */
     public int top() {
-        return top;
+        return q1.peek();
     }
 
     /** Returns whether the stack is empty. */
--- java-leetcode-solution/src/main/java/com/leetcode/solution/stack/StackUsingQueuesV3.java
+++ java-leetcode-solution/src/main/java/com/leetcode/solution/stack/StackUsingQueuesV3.java
@@ -8,37 +8,27 @@ import java.util.Queue;
  * @description
  * @date 2019-08-26 17:26
  */
-public class StackUsingQueuesV2 {
+public class StackUsingQueuesV3 {
 
     private Queue<Integer> q1 = new LinkedList<>();
 
-    private Queue<Integer> q2 = new LinkedList<>();
-
-    private int top;
-
     /** Initialize your data structure here. */
-    public StackUsingQueuesV2() {
+    public StackUsingQueuesV3() {
     }
 
     /** Push element x onto stack. */
     public void push(int x) {
-        q2.add(x);
-        top = x;
-        while (!q1.isEmpty()) {
-            q2.add(q1.remove());
+        q1.add(x);
+        int sz = q1.size();
+        while (sz > 1) {
+            q1.add(q1.remove());
+            sz--;
         }
-        Queue<Integer> temp = q2;
-        q2 = q1;
-        q1 = temp;
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        int result = q1.remove();
-        if (!q1.isEmpty()) {
-            top = q1.peek();
-        }
-        return result;
+        return q1.remove();
     }
 
     /** Get the top element. */
