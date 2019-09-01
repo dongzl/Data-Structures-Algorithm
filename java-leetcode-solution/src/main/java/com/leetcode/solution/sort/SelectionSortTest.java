af77eec52a05f08e77b717dae541f9081a3626cf
dongzl <dzllikelsw@163.com>
2019-09-01 20:34:46 +0800

    SelectionSort Test

--- java-leetcode-solution/src/main/java/com/leetcode/solution/sort/SelectionSortTest.java
+++ java-leetcode-solution/src/main/java/com/leetcode/solution/sort/SelectionSortTest.java
@@ -7,12 +7,14 @@ package com.leetcode.solution.sort;
  */
 public class SelectionSortTest {
 
+    // 选择排序，a表示数组，n表示数组大小
     public void selectionSort(int[] a, int n) {
         if (n <= 1) {
             return;
         }
 
         for(int i = 0; i < n - 1; ++i) {
+            // 查找最小值
             int minIndex = i;
             for (int j = i + 1; j < n; ++j) {
                 if (a[j] < a[minIndex]) {
