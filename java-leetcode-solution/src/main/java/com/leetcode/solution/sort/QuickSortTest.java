0115f4e25578145bc1eed40d68090d46dbae9a41
dongzl <dzllikelsw@163.com>
2019-09-04 19:48:39 +0800

    QuickSort

---
+++ java-leetcode-solution/src/main/java/com/leetcode/solution/sort/QuickSortTest.java
@@ -0,0 +1,63 @@
+package com.leetcode.solution.sort;
+
+/**
+ * @author dongzonglei
+ * @description
+ * @date 2019-09-03 17:48
+ */
+public class QuickSortTest {
+
+    /**
+     * 快速排序方法
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int[] quickSort(int[] array, int start, int end) {
+        if (array.length < 1 || start < 0 || end >= array.length || start > end) return null;
+        int smallIndex = partition(array, start, end);
+        if (smallIndex > start)
+            quickSort(array, start, smallIndex - 1);
+        if (smallIndex < end)
+            quickSort(array, smallIndex + 1, end);
+        return array;
+    }
+    /**
+     * 快速排序算法——partition
+     * @param array
+     * @param start
+     * @param end
+     * @return
+     */
+    public static int partition(int[] array, int start, int end) {
+        int pivot = (int) (start + Math.random() * (end - start + 1));
+        int smallIndex = start - 1;
+        swap(array, pivot, end);
+        for (int i = start; i <= end; i++)
+            if (array[i] <= array[end]) {
+                smallIndex++;
+                if (i > smallIndex)
+                    swap(array, i, smallIndex);
+            }
+        return smallIndex;
+    }
+
+    /**
+     * 交换数组内两个元素
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String args[]) throws Exception {
+        int[] array = new int[]{3, 8, 9, 20, 4, 8, 6};
+        int[] result = quickSort(array, 1, array.length);
+        System.out.println(result);
+    }
+}
